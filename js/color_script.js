bedf0904374f26136757116fcf1d7b1aab32cca4
Bpadilla18 <145296190+Bpadilla18@users.noreply.github.com>
2023-11-07 21:26:14 -0600

    Update color_script.js

--- js/color_script.js
+++ js/color_script.js
@@ -1,14 +1,14 @@
 function init (){
-	function myEventFunction(){
-	  alert('Braxton Padilla: This is a test');
-	}
+  function myEventFunction(){
+    alert('Braxton Padilla: This is a test');
+  }
 	
-	var d4 = document.getElementById('div4');
-	d4.addEventListener('click', myEventFunction);
-	d4.addEventListener('click', function(){
-	  console.log('I was Clicked!');
-	  d4.style.backgroundColor = '#C00';
-	  d4.innerHTML = '<strong>Goodbye</strong>';
-	});
+  var d4 = document.getElementById('div4');
+  d4.addEventListener('click', myEventFunction);
+  d4.addEventListener('click', function(){
+    console.log('I was Clicked!');
+    d4.style.backgroundColor = '#C00';
+    d4.innerHTML = '<strong>Goodbye</strong>';
+  });
 }
 window.addEventListener('load', init);
